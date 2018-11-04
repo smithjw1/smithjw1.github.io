32d6a17489e2a9283a8c55e1648144c64450d996
Jacob Wayne Smith <jacob@abravenew.com>
2018-11-03 22:27:54 -0700

    pwa

--- _site/js/site.js
+++ _site/js/site.js
@@ -1,3 +1,12 @@
+// Register the service worker if available.
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+        console.log('Successfully registered service worker', reg);
+    }).catch(function(err) {
+        console.warn('Error whilst registering service worker', err);
+    });
+}
+
 $('.testimonials blockquote:eq(0)').show();
 setTimeout('changeQuote(0)', 5000);
 function changeQuote(quoteNum) {
@@ -7,6 +16,6 @@ function changeQuote(quoteNum) {
 			quoteNum = 0;
 		}
 		$('.testimonials blockquote:eq('+quoteNum+')').fadeIn();
-		setTimeout('changeQuote('+quoteNum+')', 5000);	
-	});	
+		setTimeout('changeQuote('+quoteNum+')', 5000);
+	});
 }
